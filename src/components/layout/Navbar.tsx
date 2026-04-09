b32053641f32f6b81b63cc5e0d8fbab49445a4df
SPCMSK <izaurietamatiasignacio@gmail.com>
2026-04-09 16:49:08 -0400

    UI: Address empty space in PNG logo by scaling and expanding container bounds via CSS

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -20,12 +20,12 @@ export function Navbar({ storeEnabled = false }: { storeEnabled?: boolean }) {
 
                 {/* Logo */}
                 <Link href="/" className="flex items-center group">
-                    <div className="relative w-28 h-10 md:w-36 md:h-14 transition-transform duration-500 group-hover:scale-105">
+                    <div className="relative w-36 h-12 md:w-48 md:h-16 transition-transform duration-500 group-hover:scale-105">
                         <Image
                             src="/logo_taller_zero.png"
                             alt="Taller Zero Logo"
                             fill
-                            className="object-contain object-left"
+                            className="object-contain object-left scale-150 origin-left"
                             priority
                         />
                     </div>
